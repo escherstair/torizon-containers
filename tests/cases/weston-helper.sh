3702e594ec4eff64ae92ce1ac5d0f7cd914fa93b
Leonardo Held <leonardo.held@toradex.com>
2025-01-22 08:34:38 -0300

    Revert "tests: cases: Add qt6 openGL test to graphics-weston.bats"
    This tests need to be re-worked for the following reasons:
    
    1. Non-Weston setup containers in the Weston helper script.
    2. Qt6 tests on the graphics-weston tests.
    
    This reverts commit 90441e4072187bef7598bc604576c072a2710eeb.
    
    Signed-off-by: Leonardo Held <leonardo.held@toradex.com>

--- tests/cases/weston-helper.sh
+++ tests/cases/weston-helper.sh
@@ -1,81 +1,43 @@
 #!/usr/bin/env bash
 
-set_am62_variables() {
-  WESTON_RUN='docker container run -d --name=weston --net=host \
-          --cap-add CAP_SYS_TTY_CONFIG \
-          -v /dev:/dev -v /tmp:/tmp -v /run/udev/:/run/udev/ \
-          --device-cgroup-rule="c 4:* rmw" --device-cgroup-rule="c 13:* rmw" \
-          --device-cgroup-rule="c 10:223 rmw" --device-cgroup-rule="c 226:* rmw" \
-          artifactory-horw.int.toradex.com/dockerhub-proxy-horw/torizon/weston-am62:stable-rc \
-          --developer --tty=/dev/tty7 -- --debug'
-
-  GRAPHICS_TESTS_RUN='docker container run -d -it --name=graphics-tests --privileged \
-          -v /dev:/dev -v /tmp:/tmp  \
-          --device-cgroup-rule="c 226:* rmw" \
-          artifactory-horw.int.toradex.com/dockerhub-proxy-horw/torizon/graphics-tests-am62:stable-rc'
-
-  QT6_WAYLAND_TESTS_RUN='docker container run --rm -d -it --name=qt6-wayland-tests \
-          -v /tmp:/tmp -v /dev/dri:/dev/dri \
-          --device-cgroup-rule="c 226:* rmw" \
-          artifactory-horw.int.toradex.com/dockerhub-proxy-horw/torizon/qt6-wayland-tests-am62:stable-rc'
-}
-
-set_imx8_variables() {
-  WESTON_RUN='docker container run -d --name=weston --net=host \
-          --cap-add CAP_SYS_TTY_CONFIG \
-          -v /dev:/dev -v /tmp:/tmp -v /run/udev/:/run/udev/ \
-          --device-cgroup-rule="c 4:* rmw" --device-cgroup-rule="c 13:* rmw" \
-          --device-cgroup-rule="c 199:0 rmw" --device-cgroup-rule="c 10:223 rmw" \
-          --device-cgroup-rule="c 226:* rmw" --device-cgroup-rule="c 253:* rmw" \
-          artifactory-horw.int.toradex.com/dockerhub-proxy-horw/torizon/weston-imx8:stable-rc \
-          --developer --tty=/dev/tty7 -- --debug'
-
-  GRAPHICS_TESTS_RUN='docker container run -d -it --name=graphics-tests --privileged \
-          -v /dev:/dev -v /tmp:/tmp  \
-          --device-cgroup-rule="c 199:* rmw" --device-cgroup-rule="c 226:* rmw" \
-          artifactory-horw.int.toradex.com/dockerhub-proxy-horw/torizon/graphics-tests-imx8:stable-rc'
-
-  QT6_WAYLAND_TESTS_RUN='docker container run --rm -d -it --name=qt6-wayland-tests \
-          -v /tmp:/tmp -v /dev/dri:/dev/dri -v /dev/galcore:/dev/galcore \
-          --device-cgroup-rule="c 199:* rmw" --device-cgroup-rule="c 226:* rmw" \
-          artifactory-horw.int.toradex.com/dockerhub-proxy-horw/torizon/qt6-wayland-tests-imx8:stable-rc'
-}
-
-set_upstream_variables() {
-  WESTON_RUN='docker container run -d --name=weston --net=host \
-          --cap-add CAP_SYS_TTY_CONFIG \
-          -v /dev:/dev -v /tmp:/tmp -v /run/udev/:/run/udev/ \
-          --device-cgroup-rule="c 4:* rmw" --device-cgroup-rule="c 13:* rmw" \
-          --device-cgroup-rule="c 226:* rmw" --device-cgroup-rule="c 10:223 rmw" \
-          artifactory-horw.int.toradex.com/dockerhub-proxy-horw/torizon/weston:stable-rc \
-          --developer --tty=/dev/tty7 -- --debug'
-
-  GRAPHICS_TESTS_RUN='docker container run -d -it --name=graphics-tests --privileged \
-          -v /dev:/dev -v /tmp:/tmp \
-          --device-cgroup-rule="c 226:* rmw" \
-          artifactory-horw.int.toradex.com/dockerhub-proxy-horw/torizon/graphics-tests:stable-rc'
-
-  QT6_WAYLAND_TESTS_RUN='docker container run -d -it --name=qt6-wayland-tests \
-          -v /tmp:/tmp -v /dev/dri:/dev/dri \
-          --device-cgroup-rule="c 226:* rmw" \
-          artifactory-horw.int.toradex.com/dockerhub-proxy-horw/torizon/qt6-wayland-tests:stable-rc bash'
-}
-
 setup_weston() {
+  WESTON_RUN_AM62='docker container run -d --name=weston --net=host \
+        --cap-add CAP_SYS_TTY_CONFIG -v /dev:/dev -v /tmp:/tmp \
+        -v /run/udev/:/run/udev/ --device-cgroup-rule="c 4:* rmw" \
+        --device-cgroup-rule="c 13:* rmw" --device-cgroup-rule="c 226:* rmw" \
+        --device-cgroup-rule="c 10:223 rmw" \
+        artifactory-horw.int.toradex.com/dockerhub-proxy-horw/torizon/weston-am62:stable-rc \
+        --developer --tty=/dev/tty7 -- --debug'
+
+  WESTON_RUN_IMX8='docker container run -d --name=weston --net=host \
+        --cap-add CAP_SYS_TTY_CONFIG \
+        -v /dev:/dev -v /tmp:/tmp -v /run/udev/:/run/udev/ \
+        --device-cgroup-rule="c 4:* rmw" --device-cgroup-rule="c 253:* rmw" \
+        --device-cgroup-rule="c 13:* rmw" --device-cgroup-rule="c 226:* rmw" \
+        --device-cgroup-rule="c 10:223 rmw" --device-cgroup-rule="c 199:0 rmw" \
+        artifactory-horw.int.toradex.com/dockerhub-proxy-horw/torizon/weston-imx8:stable-rc \
+        --developer --tty=/dev/tty7 -- --debug'
+
+  WESTON_RUN_UPSTREAM='docker container run -d --name=weston --net=host \
+        --cap-add CAP_SYS_TTY_CONFIG -v /dev:/dev -v /tmp:/tmp \
+        -v /run/udev/:/run/udev/ --device-cgroup-rule="c 4:* rmw" \
+        --device-cgroup-rule="c 13:* rmw" --device-cgroup-rule="c 226:* rmw" \
+        --device-cgroup-rule="c 10:223 rmw" \
+        artifactory-horw.int.toradex.com/dockerhub-proxy-horw/torizon/weston:stable-rc \
+        --developer --tty=/dev/tty7 -- --debug'
+
   docker container stop weston || true
   docker container rm weston || true
 
   if [[ "$PLATFORM_FILTER" == *am62* ]]; then
-    set_am62_variables
+    DOCKER_RUN="$WESTON_RUN_AM62"
   elif [[ "$PLATFORM_FILTER" == *imx8* ]]; then
-    set_imx8_variables
+    DOCKER_RUN="$WESTON_RUN_IMX8"
   else
-    set_upstream_variables
+    DOCKER_RUN="$WESTON_RUN_UPSTREAM"
   fi
 
-  eval "$WESTON_RUN"
-  eval "$GRAPHICS_TESTS_RUN"
-  eval "$QT6_WAYLAND_TESTS_RUN"
+  eval "$DOCKER_RUN"
 
   sleep 30
 }
@@ -97,14 +59,12 @@ is_weston_running() {
 }
 
 teardown_weston() {
-  for container_name in qt6-wayland-tests graphics-tests weston; do
-    IMAGE_ID=$(docker container inspect -f '{{.Image}}' $container_name 2>/dev/null)
+  docker container stop weston || true
 
-    docker container stop $container_name || true
-    docker container rm $container_name || true
+  IMAGE_ID=$(docker container inspect -f '{{.Image}}' weston 2>/dev/null)
+  if [[ -n "$IMAGE_ID" ]]; then
+    docker image rm -f "$IMAGE_ID" || true
+  fi
 
-    if [[ -n "$IMAGE_ID" || $RM_ON_TEARDOWN == "true" ]]; then
-      docker image rm -f "$IMAGE_ID" || true
-    fi
-  done
+  docker container rm weston || true
 }
